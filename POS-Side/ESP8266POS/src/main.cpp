7b614a9bfa0529070b3c4713f11b01d4bfbb59d3
abishka <ata.abishka@gmail.com>
2022-01-25 15:40:52 +0330

    working from serial

--- POS-Side/ESP8266POS/src/main.cpp
+++ POS-Side/ESP8266POS/src/main.cpp
@@ -6,21 +6,20 @@ const char *ssid = "Ookk";
 const char *password = "a123456789";
 
 WiFiUDP Udp;
-unsigned int localUdpPort = 4210;                     // local port to listen on
+unsigned int localUdpPort = 4210; // local port to listen on
 
-IPAddress local_IP(192, 168, 25, 10);                 // Set your local_IP address
-IPAddress gateway(192, 168, 25, 34);                  // Set your Gateway IP address
+IPAddress local_IP(192, 168, 25, 10); // Set your local_IP address
+IPAddress gateway(192, 168, 25, 34);  // Set your Gateway IP address
 
-IPAddress Web_IP(192, 168, 25, 119);                 // Set your local_IP address
-unsigned int WebUdpPort = 4211;                     // local port to listen on
-
+IPAddress Web_IP(192, 168, 25, 119); // Set your local_IP address
+unsigned int WebUdpPort = 4211;      // local port to listen on
 
 IPAddress subnet(255, 255, 0, 0);
 
+char incomingPacket[255];                              // buffer for incoming packets
+char read_barcode[25]; // a reply string to send back
 
-
-char incomingPacket[255];                             // buffer for incoming packets
-char replyPacket[] = "Hi there! Got the message :-)"; // a reply string to send back
+bool stringComplete = false; // whether the string is complete
 
 void setup()
 {
@@ -43,6 +42,7 @@ void setup()
 
 void loop()
 {
+  
   int packetSize = Udp.parsePacket();
   if (packetSize)
   {
@@ -56,8 +56,42 @@ void loop()
     Serial.printf("UDP packet contents: %s\n", incomingPacket);
 
     // send back a reply, to the IP address and port we got the packet from
+  }
+  
+
+  if (stringComplete)
+  {
+
     Udp.beginPacket(Web_IP, WebUdpPort);
-    Udp.write(replyPacket);
+    Udp.write(read_barcode);
     Udp.endPacket();
+
+    // clear the string:
+    stringComplete = false;
+  }
+}
+
+unsigned char i = 0;
+void serialEvent()
+{
+  while (Serial.available())
+  {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    read_barcode[i] = inChar;
+    if(++i>20)
+    {
+      i=20;
+    }
+    read_barcode[i] = 0;
+
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n')
+    {
+      stringComplete = true;
+      i = 0;
+    }
   }
 }
--- Scanner-Side/main.py
+++ Scanner-Side/main.py
@@ -1,8 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import socket
+
+barcod_scaner_ip = "192.168.25.10"
+barcod_scaner_port = 4210
+recive_barcod_scaner_port = 4211
+
+MESSAGE = b"udp_receved"
+
 
 #put your pos url 
 my_pos_url='http://localhost'
+
 
 #put your input_html_id_for_barcode 
 input_html_id_for_barcode='scan-barcode-input'
@@ -11,10 +20,18 @@ input_html_id_for_barcode='scan-barcode-input'
 driver = webdriver.Firefox()
 
 driver.get(my_pos_url)
+
+sender_socket = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
+sender_socket.sendto(MESSAGE, (barcod_scaner_ip, barcod_scaner_port))
+
+
+reciver_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+reciver_socket.bind(('0.0.0.0', recive_barcod_scaner_port))
 while(True):
-    a=input("ENTER pr_cod:")
-    b=input("ENTER number:")
-    b=int(b)
-    driver.find_element_by_id(input_html_id_for_barcode).send_keys(a)
-    for i in range(b):
-        driver.find_element_by_id(input_html_id_for_barcode).send_keys(Keys.ENTER)
+    data, addr = reciver_socket.recvfrom(128)  # buffer size is 1024 bytes
+    print( data.decode('utf-8'))
+    sender_socket.sendto(MESSAGE, (barcod_scaner_ip, barcod_scaner_port))
+    driver.find_element_by_id(input_html_id_for_barcode).clear()
+    driver.find_element_by_id(input_html_id_for_barcode).send_keys(data.decode('utf-8'))
+    driver.find_element_by_id(input_html_id_for_barcode).send_keys(Keys.ENTER)
